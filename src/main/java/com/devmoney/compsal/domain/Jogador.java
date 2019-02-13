b8ede53874f22c1aac9724716f4bfd7f1428fe77
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-02-13 21:01:04 -0200

    Implementado o bando de dados H2 e criada a classe 'Jogador'

---
+++ src/main/java/com/devmoney/compsal/domain/Jogador.java
@@ -0,0 +1,63 @@
+package com.devmoney.compsal.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Jogador implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String nome;
+	
+	public Jogador() {
+	}
+	
+	public Jogador(int id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Jogador other = (Jogador) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+		
+}
--- src/main/java/com/devmoney/compsal/resources/JogadorResource.java
+++ src/main/java/com/devmoney/compsal/resources/JogadorResource.java
@@ -1,16 +1,28 @@
 package com.devmoney.compsal.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devmoney.compsal.domain.Jogador;
 
 @RestController
 @RequestMapping(value="/jogadores")
 public class JogadorResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String texto() {
-		return "Rest '/jogadores' está funcionando!";
+	public List<Jogador> listar() {
+		Jogador jog1 = new Jogador(1, "Rivaldo");
+		Jogador jog2 = new Jogador(2, "Cristian");
+		
+		List<Jogador> lista = new ArrayList<>();
+		lista.add(jog1);
+		lista.add(jog2);
+		
+		return lista;
 	}
 	
 }
