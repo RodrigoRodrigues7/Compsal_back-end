57ffc8c536eee1f890ae335ad5f6f6e91f976081
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-02-13 20:10:01 -0200

    First commit

---
+++ src/main/java/com/devmoney/compsal/CompsalBackendApplication.java
@@ -0,0 +1,14 @@
+package com.devmoney.compsal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompsalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompsalBackendApplication.class, args);
+	}
+
+}
+
